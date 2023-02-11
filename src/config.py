15fbd836aced96b5af343b7056ec552047b001d1
mlataza <mlataza@gmail.com>
2023-02-11 14:13:12 +0800

    Add MySQL credentials as environment variable

--- src/config.py
+++ src/config.py
@@ -3,14 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from flask_cors import CORS
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 
 # Flask-SQLAlchemy configuration keys. See: https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
-# TODO: Store creds as environment variable
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://root:p%40ssw0rd!@localhost:3306/todoist-clone"
+] = f"mysql+pymysql://{os.environ['MYSQL_USER']}:{os.environ['MYSQL_PASS']}@localhost:3306/todoist-clone"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
