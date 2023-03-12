18a6e6ace348ffc79b776673b2b40a329d525898
Bruce Baynosa <bruce.baynosa@asggroup.com.au>
2023-03-12 23:07:16 +0800

    reverted db config

--- src/config.py
+++ src/config.py
@@ -3,19 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from flask_cors import CORS
 from flask_migrate import Migrate
-import urllib.parse
-import logging
-
-password = urllib.parse.quote_plus("j%Y9%E8t5nE$U9")
 
 app = Flask(__name__)
-
 
 # Flask-SQLAlchemy configuration keys. See: https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
 # TODO: Store creds as environment variable
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = f"mysql+pymysql://flask-api:{password}@192.168.21.10:3306/todoist-clone"
+] = "mysql+pymysql://root:p%40ssw0rd!@localhost:3306/todoist-clone"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -24,5 +19,3 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 CORS(app)
-
-logging.getLogger('flask_cors').level = logging.DEBUG
